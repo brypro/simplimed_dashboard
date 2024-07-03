95eabdf5ea2bcb96e3ec83fa66e6607cae0765ad
Natalia Mellado <nataliamelladocani@gmail.com>
2024-07-03 01:02:24 -0400

    Refactor examenes page and added formExamenes component

--- src/app/examenes/page.tsx
+++ src/app/examenes/page.tsx
@@ -1,6 +1,6 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import TableThree from "@/components/Tables/TableThree";
-import InputGroup from "@/components/FormElements/InputGroup";
+import TableThree from "./components/TableThree";
+import { FormExamenes } from "./components/formExamenes";
 
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLaout";
@@ -22,60 +22,7 @@ const TablesPage = () => {
                 Añadir Examen
               </h3>
             </div>
-            <form action="#">
-              <div className="p-5">
-
-                <InputGroup
-                  label="Nombre"
-                  type="text"
-                  placeholder="Ingresa nombre y apellido"
-                  customClasses="mb-3"
-                  required
-                />
-
-                <InputGroup
-                  label="Tipo"
-                  type="text"
-                  placeholder="Ingresa el tipo de exámen"
-                  customClasses="mb-3"
-                  required
-                />
-
-                <InputGroup
-                  label="Valor"
-                  type="number"
-                  placeholder="Ingresa el valor del exámen"
-                  customClasses="mb-3"
-                  required
-                />
-
-                <InputGroup
-                  label="Duración"
-                  type="text"
-                  placeholder="Ingresa la duración del exámen"
-                  customClasses="mb-3"
-                  required
-                />
-
-                <InputGroup
-                  label="Preparación previa"
-                  type="text"
-                  placeholder="Ingresa la preparación previa del exámen"
-                  customClasses="mb-3"
-                  required
-                />
-            
-                <div className="flex space-x-4 ">
-                  <button className="my-1 py-1.5 flex w-full justify-center rounded-[7px] border border-primary font-medium dark:text-white hover:bg-opacity-90">
-                    Limpiar
-                  </button>
-                  <button className="my-1 flex py-1.5 w-full justify-center rounded-[7px] bg-primary font-medium text-white hover:bg-opacity-90">
-                    Guardar
-                  </button>
-                </div>
-
-              </div>
-            </form>
+            <FormExamenes />
           </div>
     
       <div className="flex flex-col gap-10">
