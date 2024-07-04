e2f8cb410c52a7bc1e1ea7e0c2c97752bfad4749
Natalia Mellado <nataliamelladocani@gmail.com>
2024-07-04 17:09:49 -0400

    Refactor logout

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -9,7 +9,7 @@ const DropdownUser = () => {
   const { data: session } = useSession();
 
   const handleLogout = async () => {
-    await signOut();
+    await signOut({ callbackUrl: "/" });
   };
 
   return (
@@ -86,7 +86,8 @@ const DropdownUser = () => {
 
           <div className="p-2.5">
             <button
-              onClick={() => signOut()} 
+              onClick={handleLogout}
+
               className="flex w-full items-center gap-2.5 rounded-[7px] p-2.5 text-sm font-medium text-dark-4 duration-300 ease-in-out hover:bg-gray-2 hover:text-dark dark:text-dark-6 dark:hover:bg-dark-3 dark:hover:text-white lg:text-base"
             >
               <svg
